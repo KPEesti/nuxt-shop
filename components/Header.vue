2a4a7b151763d784f3c72773ff5505ab90504b32
So_Green <iwantoseeyou1@gmail.com>
2023-06-07 12:29:02 +0500

    Немного обновил Header.vue Добавил место под логотип, а так же иконки корзины и личного кабинета.
    Немного обновил вёрстку для ProductList.vue. Теперь корректно отображается на любом устройтсве

--- components/Header.vue
+++ components/Header.vue
@@ -1,14 +1,23 @@
 <template>
-  <div class="flex justify-around	w-8/12 m-auto p-5">
-    <NuxtLink to="/">
-      Главная
-    </NuxtLink>
-    <NuxtLink to="/catalog">
-      Каталог
-    </NuxtLink>
-    <NuxtLink to="/contacts">
-      Контакты
-    </NuxtLink>
+  <div class="flex justify-between w-10/12 m-auto p-5">
+    <div>
+      <h2>Logo</h2>
+    </div>
+    <div class="flex justify-between w-4/12">
+      <NuxtLink to="/">
+        Главная
+      </NuxtLink>
+      <NuxtLink to="/catalog">
+        Каталог
+      </NuxtLink>
+      <NuxtLink to="/contacts">
+        Контакты
+      </NuxtLink>
+    </div>
+    <div class="flex justify-between w-1/12">
+      <img src="/cart-2-svgrepo-com.svg" alt="" class="w-7">
+      <img src="/person-svgrepo-com.svg" alt="" class="w-7">
+    </div>
   </div>
 </template>
 
--- components/ProductList.vue
+++ components/ProductList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid grid-cols-1 gap-10 md:grid-cols-2 2xl:grid-cols-3 ">
+  <div class="grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4">
     <ProductItem :product="product"/>
     <ProductItem :product="product"/>
     <ProductItem :product="product"/>
@@ -13,7 +13,7 @@ export default {
   name: "ProductList",
   setup() {
     const product = {
-      src: "img/jewelry_1.jpg",
+      src: "img/1.1.jpg",
       title: "Моно-серьга Звезда",
       id: 1,
       price: 10800,
